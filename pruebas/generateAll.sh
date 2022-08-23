27aad7e0dea930405d03e7f188e757930a64b907
Sergio Salinas <ssalinasfe@gmail.com>
2022-08-23 14:41:47 -0400

    added generateAll.sh for generate triangulation and mesh

---
+++ pruebas/generateAll.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+file_folder="output/"
+read -p "Enter start number " i
+
+while [ $i -le 35000000 ]
+do
+    num_vertices="${i}"
+    node_file="points_${num_vertices}.1.node"
+    ele_file="points_${num_vertices}.1.ele"
+    neigh_file="points_${num_vertices}.1.neigh"
+    output="points_${num_vertices}.1"
+
+    echo -n "Generating ${i} random points"
+    (eval "python3 randomPoints.py $i > points_$i.node && triangle -zn points_$i.node")
+    (eval "rm points_$i.node")
+
+
+    echo -n "Generating mesh with ${i} vertices "
+    ./compactPolylla ${node_file} ${ele_file} ${neigh_file} ${file_folder}${output}
+    i=$(($i + 1000000 ))
+
+done
